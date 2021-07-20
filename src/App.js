9abe7d699b87dea2cfe030efc7b926e77a85b4a0
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-07-20 21:01:09 +0200

    Add images object paths and 13th image

--- src/App.js
+++ src/App.js
@@ -6,7 +6,61 @@ import "./styles/App.scss";
 const App = () => {
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
-  
+  const [images, setImages] = useState([
+    {
+      path: require("./assets/1.jpg").default,
+      alt: "New Levels New Devils",
+    },
+    {
+      path: require("./assets/2.jpg").default,
+      alt: "Technicolor",
+    },
+    {
+      path: require("./assets/3.jpg").default,
+      alt: "Effloresce",
+    },
+    {
+      path: require("./assets/4.jpg").default,
+      alt: "Newborn Sun",
+    },
+    {
+      path: require("./assets/5.jpeg").default,
+      alt: "The Most Hated",
+    },
+    {
+      path: require("./assets/6.jpg").default,
+      alt: "For Long Tomorrow",
+    },
+    {
+      path: require("./assets/7.jpeg").default,
+      alt: "Grow",
+    },
+    {
+      path: require("./assets/8.jpg").default,
+      alt: "Filoxiny",
+    },
+    {
+      path: require("./assets/9.jpg").default,
+      alt: "Sleep Dirt",
+    },
+    {
+      path: require("./assets/10.jpg").default,
+      alt: "Bottle It In",
+    },
+    {
+      path: require("./assets/11.jpg").default,
+      alt: "Broken Brights",
+    },
+    {
+      path: require("./assets/12.jpg").default,
+      alt: "Con Todo El Mundo",
+    },
+    {
+      path: require("./assets/13.jpg").default,
+      alt: "Chon",
+    },
+  ]);
+
   return (
     <div>
       <ScoreCard currentScore={currentScore} bestScore={bestScore} />
