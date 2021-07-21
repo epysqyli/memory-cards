619be9c94d95b4e496125b9001988d1fbe4bd1e4
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-07-21 15:00:41 +0200

    Reorganize navbar content

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -6,8 +6,14 @@ const ScoreCard = (props) => {
     <div className="scorecard">
       <div className="logo">test your mem</div>
       <div className="scores">
-        <div className="current-score">current score: {props.currentScore}</div>
-        <div className="best-score">best score: {props.bestScore}</div>
+        <div className="current-score">
+          <p>current score</p>
+          <div>{props.currentScore}</div>
+        </div>
+        <div className="best-score">
+          <p>best score</p>
+          <div>{props.bestScore}</div>
+        </div>
       </div>
     </div>
   );
